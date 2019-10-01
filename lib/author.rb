425c6fc38b975b1a5347fb64264c1a12408c12da
Chris Kerbo <chris.kerbo@outlook.com>
2019-10-01 17:20:11 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,22 @@
+class Author
+
+  attr_accessor :name
+
+  def initialize(name)
+    @name = name
+  end
+
+  def add_post(post)
+    self.author = post
+  end
+
+  def add_post_by_title(title)
+    post = Author.new(title)
+    add_post(post)
+  end
+
+  def self.count
+    Author.all.count
+  end
+
+end
